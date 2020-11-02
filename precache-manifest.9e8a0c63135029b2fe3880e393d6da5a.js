e37dcc416363ac059f985dc4f00cb4893ffabfbd
Asif Mohammed <asifm2882@gmail.com>
2020-11-02 20:14:15 +0400

    Updates

---
+++ precache-manifest.9e8a0c63135029b2fe3880e393d6da5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748f9dc686673236148a5beb55d7ee7f",
+    "url": "/react-spaces-ui/index.html"
+  },
+  {
+    "revision": "a2f6b853ce3a2753b340",
+    "url": "/react-spaces-ui/static/css/main.45630d3e.chunk.css"
+  },
+  {
+    "revision": "22d9e371e9106be847b4",
+    "url": "/react-spaces-ui/static/js/2.fec15151.chunk.js"
+  },
+  {
+    "revision": "78e39aa0fb7ea65d9581f30f7edb60fb",
+    "url": "/react-spaces-ui/static/js/2.fec15151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f6b853ce3a2753b340",
+    "url": "/react-spaces-ui/static/js/main.bab41eb4.chunk.js"
+  },
+  {
+    "revision": "c8bb0af00fd047236d43",
+    "url": "/react-spaces-ui/static/js/runtime-main.8db45d17.js"
+  }
+]);
