1f1c539ab5fc6d57feddea075614128276650c9c
Asif Mohammed <asifm2882@gmail.com>
2020-11-04 22:07:59 +0400

    add drag contrain to motion slider

--- src/components/MotionSlider/Track.tsx
+++ src/components/MotionSlider/Track.tsx
@@ -8,9 +8,15 @@ const Track = ({ children }) => {
   const { state, dispatch } = useContext(sliderContext)
   const controls = useAnimation()
   const inverted = useInvertedScale()
-  console.log(state.items)
+  // console.log(state.items)
   const negativeItems = state.items.map((item) => item * -1)
 
+  const calcSliderMargin = () => {
+    const appContainerWidth =
+      document.querySelector(".App-container")?.clientWidth ?? 560
+    const sliderCardWidth = 300
+    return (appContainerWidth - sliderCardWidth) / 2
+  }
   useEffect(() => {
     controls.start("visible")
   }, [controls])
@@ -20,31 +26,14 @@ const Track = ({ children }) => {
   }
 
   function onDragEnd(event, info) {
-    // console.log(info)
-    const offset = info.offset.x
-    const correctedVelocity = info.velocity.x * 0.3
-
-    const direction = correctedVelocity < 0 || offset < 0 ? 1 : -1
-    const startPosition = info.point.x - offset
-
-    const endOffset = offset
-    direction === 1
-      ? Math.min(correctedVelocity, offset)
-      : Math.max(correctedVelocity, offset)
-    const endPosition = startPosition + endOffset
-
+    const endPosition = info.point.x
     const closestPosition = negativeItems.reduce((prev, curr) =>
       Math.abs(curr - endPosition) < Math.abs(prev - endPosition) ? curr : prev
     )
-
-    console.log(closestPosition)
-
     const activeSlide = negativeItems.indexOf(closestPosition)
-
     dispatch({ type: "SET_ACTIVE_ITEM", activeItem: activeSlide })
-
     controls.start({
-      x: closestPosition + 112,
+      x: closestPosition,
       transition: openSpring,
     })
   }
@@ -53,14 +42,14 @@ const Track = ({ children }) => {
       className="Flickity-container"
       variants={flicktyVariants}
       drag={"x"}
-      //   onDragEnd={onDragEnd}
+      onDragEnd={onDragEnd}
       dragConstraints={{
-        left: -(768 / 1.5),
+        left: -(1000 / 1.5),
         right: 0,
       }}
-      style={{ ...inverted }}
+      style={{ ...inverted, marginLeft: calcSliderMargin() }}
       initial={"hidden"}
-      animate={"visible"}
+      animate={controls}
       exit={"hidden"}
       transition={openSpring}
       onAnimationComplete={onComplete}
